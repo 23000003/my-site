6e53e6f0fc1c385c75e5882904802957b9234439
23000003 <kurtdicdican@gmail.com>
2024-08-25 21:44:10 +0800

    #

--- src/helper/config.ts
+++ src/helper/config.ts
@@ -1,4 +1,7 @@
-const dev = process.env.NODE_ENV !== "production" || "Production";
+const dev = process.env.NODE_ENV !== "production";
+
+console.log(process.env.NODE_ENV);
+console.log(dev);
 
 export const server = dev
   ? "http://localhost:3000"
