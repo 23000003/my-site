91ec5ab1074a1fa3c1a1ad347df8ffbbbe2953c7
23000003 <kurtdicdican@gmail.com>
2024-08-25 21:36:29 +0800

    BUG FIXED

--- src/helper/config.ts
+++ src/helper/config.ts
@@ -1,5 +1,5 @@
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== "production" || "Production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://my-site-mocha-three.vercel.app/";
+  : "https://my-site-mocha-three.vercel.app";
